baa90f9a79aa742e327eaf9c6fbfa74074a7df1d
Muksud Hussain Mahi <mmahi1@mycentennialcollege.ca>
2022-08-31 17:04:41 -0400

    Customer and movie api

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+using AutoMapper;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IEnumerable<CustomerDto>GetCustomers()
+        {
+            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+        }
+
+        // GET /api.customers/id
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                NotFound();
+
+            return Ok(Mapper.Map<Customer,CustomerDto>(customer));
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                BadRequest();
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id; 
+
+            return Created(Request.RequestUri+"/"+customer.Id, customerDto);
+        }
+
+        // PUT /api/customers/id
+        [HttpPut]
+        public void UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customerToUpdate = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if(customerToUpdate == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            Mapper.Map(customerDto, customerToUpdate);
+
+            _context.SaveChanges();
+        }
+
+        // DETELE /api/customers/id
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerToDelete = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerToDelete == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Customers.Remove(customerToDelete);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using AutoMapper;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET /api/movies
+        public IHttpActionResult GetMovies()
+        {
+            return Ok(_context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>));
+        }
+
+        //GET /api/movies/id
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return NotFound();
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+        //POST /api/movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            movieDto.DateAdded = DateTime.Now;
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            movieDto.Id = movie.Id;
+            return Created(Request.RequestUri + "/" + movie.Id, movieDto);
+        }
+
+        //PUT /api/movies/id
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var movieToUpdate = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movieToUpdate == null)
+                return NotFound();
+            Mapper.Map(movieDto, movieToUpdate);
+            _context.SaveChanges();
+            return Ok("Movie Updated");
+
+        }
+        //DELETE /api/movies/id
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            var movieToDelete = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movieToDelete == null)
+                return NotFound();
+            _context.Movies.Remove(movieToDelete);
+            _context.SaveChanges();
+            return Ok("Movie Deleted");
+        }
+    }
+}
